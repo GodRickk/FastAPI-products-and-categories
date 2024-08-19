2adb0178dd17080576dc07612b853b9068fcd761
GodRickk <72164370+GodRickk@users.noreply.github.com>
2024-08-19 19:52:35 +0300

    feat: add Pydantic models

---
+++ schemas.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+class ProductBase(BaseModel):
+    price: int
+    amount: int
+    name: str
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class Product(ProductBase):
+    id: int
+    category_id: int
+
+    class Config:
+        orm_mode = True
+
+
+
+class ProductCategoryBase(BaseModel):
+    name: str
+
+
+class ProductCategoryCreate(ProductCategoryBase):
+    pass
+
+
+class ProductCategory(ProductCategoryBase):
+    id: int
+    products: list[Product] = []
+
+    class Config:
+        orm_mode = True
